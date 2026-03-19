4e197b5bca80c00f1b84afa97a74eb357e4e84f9
idsdk <idsdk@gmail.com>
2026-03-19 16:24:41 +0900

    View 속성, 리스너, 기본위젯의 다양한 속성들

--- app/src/main/java/kr/ac/kopo/buttontest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/buttontest/MainActivity.java
@@ -1,6 +1,10 @@
 package kr.ac.kopo.buttontest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edit1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main2);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        Button btn1 = findViewById(R.id.btn1);
+        edit1 = findViewById(R.id.edit1);
+        btn1.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String name = edit1.getText().toString();
+            Toast.makeText(getApplicationContext(), name + "님 환영합니다.", Toast.LENGTH_LONG).show();
+        }
+    };
 }
